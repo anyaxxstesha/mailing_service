264c2f47ebf0214c9c0ae95d173bed526e59cf0f
Anna Tarazanova <an.tarazanova2016@yandex.ru>
2024-07-23 21:33:50 +0300

    fix bug

--- mailings/urls.py
+++ mailings/urls.py
@@ -7,9 +7,9 @@ from mailings.views import MessageListView, MessageDetailView, MessageCreateView
 app_name = MailingsConfig.name
 
 urlpatterns = [
-    path('', MessageListView.as_view(), name='message_list'),
-    path('mailings/<int:pk>/', MessageDetailView.as_view(), name='message_detail'),
-    path('mailings/create/', MessageCreateView.as_view(), name='message_create'),
-    path('mailings/<int:pk>/update/', MessageUpdateView.as_view(), name='message_update'),
-    path('mailings/<int:pk>/delete/', MessageDeleteView.as_view(), name='message_delete'),
+    path('message', MessageListView.as_view(), name='message_list'),
+    path('message/<int:pk>/', MessageDetailView.as_view(), name='message_detail'),
+    path('message/create/', MessageCreateView.as_view(), name='message_create'),
+    path('message/<int:pk>/update/', MessageUpdateView.as_view(), name='message_update'),
+    path('message/<int:pk>/delete/', MessageDeleteView.as_view(), name='message_delete'),
 ]
